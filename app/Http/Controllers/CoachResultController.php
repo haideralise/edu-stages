779f3045ef33b650420229a0ccf8aaeefd3a2ec5
Haider Ali <se.haiderali@gmail.com>
2026-03-27 09:21:40 +0500

    Remove AssesService stub, query EduResult directly in controllers

--- app/Http/Controllers/CoachResultController.php
+++ app/Http/Controllers/CoachResultController.php
@@ -5,16 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\EduClassUser;
 use App\Models\EduResult;
 use App\Models\WpUser;
-use App\Services\AssesService;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CoachResultController extends Controller
 {
-    public function __construct(
-        private readonly AssesService $assesService,
-    ) {}
-
     public function index(Request $request): View
     {
         $this->authorize('viewAsCoach', EduResult::class);
@@ -23,13 +18,10 @@ class CoachResultController extends Controller
 
         $studentIds = EduClassUser::studentIdsForTeacher($user->ID);
 
-        // Get results for those students via AssesService
-        $results = $this->assesService->getResultsForStudents($studentIds->all());
+        $results = EduResult::whereIn('user_id', $studentIds->all())->get();
 
-        // Group by student
         $resultsByStudent = $results->groupBy('user_id');
 
-        // Get student names
         $students = WpUser::whereIn('ID', $studentIds)->get()->keyBy('ID');
 
         return view('coach.results', compact('resultsByStudent', 'students'));
